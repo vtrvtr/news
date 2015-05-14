ad14405ba7028a00b35bb917faace0a5c11ef7c8
Vitor Hugo <vtr@hotmail.com.br>
2015-05-13 22:13:08 -0300

    organized categories

--- get_articles.py
+++ get_articles.py
@@ -5,18 +5,31 @@ from pathlib import Path
 news_folder = Path("E:\\", "code", 'news')
 
 sites = {'exame': 'http://exame.abril.com.br/',
-              'g1': 'http://g1.globo.com/',
-              'uol': 'http://www.uol.com.br', 
-              'terra': 'http://www.terra.com'}
+         'g1': 'http://g1.globo.com/',
+         'uol': 'http://www.uol.com.br',
+         'terra': 'http://www.terra.com'}
+
+
+categories = {'exame': ['/tecnologia/',
+                        '/negocios/',
+                        '/economia/'],
+              'uol': ['economia.uol',
+                      'tecnologia.uol',
+                      'mulher.uol',
+                      'celebridades.uol'],
+              'g1': ['/brasil/',
+                     '/politica/',
+                     '/ciencia-e-saude/'],
+              'terra': ['tecnologia.terra',
+                        'mulher.terra',
+                        'economia.terra',
+                        '/brasil/']}
+
 
 news = newspaper.build(
-    sites['g1'], language='pt', memoize_articles=False, fetch_images=False)
-
+    sites['exame'], language='pt', memoize_articles=False, fetch_images=False)
 
 
 for art in news.articles:
-    if '/mundo/' in str(art.url):
-        print(art.title.encode('utf-8')) 
-
-
-
+    if any(category in str(art.url) for category in categories['exame']):
+        print(art.url.encode('utf-8'))
