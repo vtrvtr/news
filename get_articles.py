3ba053d80e48c35943b3a0b5cb258f015382ae3a
Vitor Hugo <vtr@hotmail.com.br>
2015-06-30 20:11:12 -0300

    checking for uniques

--- get_articles.py
+++ get_articles.py
@@ -1,6 +1,11 @@
+# -*- coding: utf-8 -*-
 import newspaper
 from pathlib import Path
-
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import inspect
+from collections import Counter
 
 news_folder = Path("E:\\", "code", 'news')
 
@@ -26,10 +31,57 @@ categories = {'exame': ['/tecnologia/',
                         '/brasil/']}
 
 
-news = newspaper.build(
-    sites['exame'], language='pt', memoize_articles=False, fetch_images=False)
+engine = create_engine('sqlite:///E:\\code\\news\\articles.db', echo=True)
+base = declarative_base()
+Session = sessionmaker(bind=engine)
 
 
-for art in news.articles:
-    if any(category in str(art.url) for category in categories['exame']):
-        print(art.url.encode('utf-8'))
+class Article(base):
+    __tablename__ = 'articles'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    n_chars = Column(Integer)
+    company = Column(String)
+    url = Column(String)
+    body = Column(String)
+
+    def __repr__(self):
+        # return self.url
+        return u'id: {} \ntitle: {} \nurl: {} \nNumber of chars: {} \nbody: {}...'.format(self.id, self.title, self.url, self.n_chars, self.body[:100]).encode('utf-8')
+
+base.metadata.create_all(engine)
+
+
+add = False
+# outlet_option = 'g1'
+session = Session()
+# def add_articles(articles, outlet_option, sites, categories):
+
+if add:
+    for outlet_option in sites:
+        news = newspaper.build(sites[outlet_option], language='pt', memoize_articles=False,
+                               fetch_images=False)
+        for art in news.articles:
+            # only accepts links from relevant categories
+            if any(category in str(art.url) for category in categories[outlet_option]):
+                try:
+                    art.download()
+                    art.parse()
+                    chars_count = Counter(art.text.encode('utf-8'))
+                    # don't let the same link be added twich
+                    for unique_url in session.query(Article).filter(Article.url == art.url):
+                        if unique_url is not 0:
+                            break
+                        else:
+                            session.add(Article(title=art.title, n_chars=sum(
+                                chars_count.values()), company=outlet_option, url=art.url, body=art.text))
+                except Exception as e:
+                    pass
+        session.commit()
+
+
+total = 0
+for n in session.query(Article).filter(Article.url == 'http://exame.abril.com.br/negocios/noticias/grupo-com-sede-nos-eua-eleva-participacao-em-acoes-da-vale'):
+    print n.id
+    break
